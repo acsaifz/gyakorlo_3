adce757a39573fcc6270793cf36b6ac257dce3d1
AcsaiFZ <acsai.fz@gmail.com>
2022-03-13 11:09:34 +0100

    clean

--- src/hu/progmatic/szamjegyoszegzo.java
+++ src/hu/progmatic/szamjegyoszegzo.java
@@ -1,4 +1,15 @@
 package hu.progmatic;
 
 public class szamjegyoszegzo {
+    public static void main(String[] args) {
+        int number = 123456;
+        int sumDigits = 0;
+
+        do {
+            sumDigits += Math.abs(number) % 10;
+            number = number/10;
+        }while(number != 0);
+
+        System.out.println("Számjegyek összege: " + sumDigits);
+    }
 }
