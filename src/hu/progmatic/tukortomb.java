2c27c8e46bca8d7635d27819507df58807769f4c
AcsaiFZ <acsai.fz@gmail.com>
2022-03-13 11:59:42 +0100

    Code simplification

--- src/hu/progmatic/tukortomb.java
+++ src/hu/progmatic/tukortomb.java
@@ -3,7 +3,7 @@ package hu.progmatic;
 public class tukortomb {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 2, 3, 4, 5, 5, 4, 3, 2, 2, 1};
-        boolean isMirror = numbers.length == 0 ? false : true;
+        boolean isMirror = numbers.length != 0;
 
         for (int i = 0; i < numbers.length/2;i++){
             int lastIndex = numbers.length-1-i;
